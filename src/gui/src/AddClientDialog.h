bf89b0b97d8098435d6ebbb8e1485f378818ec72
Wesley Sousa <wesleyyps@gmail.com>
2026-07-26 13:28:31 -0300

    refactor: add missing cstdint includes, fix double const declarations, and replace NULL with nullptr

--- src/gui/src/AddClientDialog.h
+++ src/gui/src/AddClientDialog.h
@@ -41,8 +41,8 @@ public:
     AddClientDialog(const QString& clientName, QWidget* parent = 0);
     ~AddClientDialog();
 
-    int addResult() const const { return m_AddResult; }
-    bool ignoreAutoConfigClient() const const { return m_IgnoreAutoConfigClient; }
+    int addResult() const { return m_AddResult; }
+    bool ignoreAutoConfigClient() const { return m_IgnoreAutoConfigClient; }
 
 protected:
     void changeEvent(QEvent *e);
